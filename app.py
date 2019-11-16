4ad3ef1f08d2d9cb1d93a9416fefdb7b37086e89
Baynova Ekaterina <ekaterinabainova@gmail.com>
2019-11-16 18:44:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    #!/usr/bin/env python3
+
+    print("Content-type: text/html")
+    print()
+    print("<h1>Hello world!</h1>")
     return "Hello World!"
