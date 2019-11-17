b38cf3bb5848af67eff58f062c96a1f0d5b28dce
Baynova Ekaterina <ekaterinabainova@gmail.com>
2019-11-17 14:05:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+import subprocess
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+	return render_template('index.html')
+@app.route('/process_data/', methods=['POST'])
+def script():
+	#return "Hello"    	
+	subprocess.run("sh ./script.sh", shell = True)
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
