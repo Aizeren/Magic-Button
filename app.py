477bc510aaee98c41effcfdfb3ba79187d851d95
Baynova Ekaterina <ekaterinabainova@gmail.com>
2019-11-19 14:17:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ def main():
 	return render_template('index.html')
 @app.route('/process_data/', methods=['POST'])
 def script():
+	subprocess.run("sh ./script.sh", shell = True)
 	a = request.form['a']
 	b = request.form['b']
 	return str(float(a)+float(b))
