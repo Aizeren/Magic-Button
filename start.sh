2b95562d2f925748b805deed27a270b4f75358b8
Ekaterina Bainova <ekaterinabainova@gmail.com>
2019-12-01 17:52:05 +0300

    Update

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 mkdir ~/.azure
 touch ~/.azure/credentials
 echo -en "[default]\nsubscription_id=$subscription_id\nclient_id=$client_id\nsecret=$secret\ntenant=$tenant">~/.azure/credentials
+echo -en "[default]\nsubscription_id=$subscription_id\nclient_id=$client_id\nsecret=$secret\ntenant=$tenant">~/.azure/credentials
 
 pip3 install ansible[azure]
 curl -sL https://aka.ms/InstallAzureCLIDeb | bash
