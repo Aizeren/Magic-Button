37457fc8a0e9a17a91e86374802ac485779190e0
Ekaterina Bainova <ekaterinabainova@gmail.com>
2019-12-01 16:14:02 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
+mkdir ~/.azure
 touch ~/.azure/credentials
 echo -en "[default]\nsubscription_id=$subscription_id\nclient_id=$client_id\nsecret=$secret\ntenant=$tenant">~/.azure/credentials
-pip3 install -r requirements.txt
+pip3 install -r additionalReq.txt
 curl -sL https://aka.ms/InstallAzureCLIDeb | bash
-
