58a87a0ff3307284d0ed6b01bcbf24b65a66cd2d
Ekaterina Bainova <ekaterinabainova@gmail.com>
2019-12-01 15:26:02 +0300

    Added variables for VM creating and credentials

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask, render_template, request
+import os
+
 import subprocess
+from flask import Flask, render_template, request
+
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def main():
+	#print(os.environ)
 	return render_template('index.html')
+
+
+@app.route("/test/", methods=['POST'])
+def test():
+	a = request.form["a"]
+	b = request.form["b"]
+	return render_template('test.html', a = a, b = b)
+
+
 @app.route('/process_data/', methods=['POST'])
 def script():
-	return str(subprocess.call("sh ./process_data.sh", shell = True))
+	createVMreturn = subprocess.call("sh ./process_data.sh", shell = True)
+	return str(createVMreturn)
+
 
 if __name__ == "__main__":
 	app.run()
--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
+touch ~/.azure/credentials
+echo -en "[default]\nsubscription_id=$subscription_id\nclient_id=$client_id\nsecret=$secret\ntenant=$tenant">~/.azure/credentials
 pip3 install -r requirements.txt
 curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+
