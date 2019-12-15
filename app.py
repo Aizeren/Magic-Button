ada284e52f112a6fd80ed845477f748dd65f95f7
Ekaterina Bainova <ekaterinabainova@gmail.com>
2019-12-16 01:38:05 +0300

    Fixed another one stupid error

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ def process_data():
 	#config hosts
 	os.system("mkdir /etc/ansible")
 	os.system("echo \"vm ansible_host="+nodeIP+" ansible_ssh_user=$admin_username ansible_ssh_pass=$admin_password \">/etc/ansible/hosts")
+	#os.system("echo \"vm ansible_host="+nodeIP+" ansible_ssh_user=$panda ansible_ssh_pass=$superior0@mail.ru \">/etc/ansible/hosts")
 	
 	#install requiered packages
 	os.system("ansible-playbook ./scripts/installPacksOnVM.yml")
